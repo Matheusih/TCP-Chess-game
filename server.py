9a0b5ee6d33e7fcfa981ed93fe9ded348ecf5bb6
Matheus Hertzog <mihertzog@inf.ufpel.edu.br>
2018-07-14 19:25:10 -0300

    Core message loop established

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import socket
 import threading
+import pickle
+import sys
 from match import Match
 from player import Player
 from arguments import get_args
@@ -16,7 +18,11 @@ class Server:
 
         # Reserving server side address #
         self.bind()
-
+        
+    def close(self):
+        # Close the socket
+        self.sock.close()
+        
     def bind(self):
         self.sock.bind((self.args.host, self.args.port))
         self.sock.listen(1)
@@ -47,20 +53,40 @@ class Server:
             self.players.append(connection1)
             print(self.players)
 
+    
     def handler(self, chessMatch):
+        p1c = chessMatch.p1.connection
+        p2c = chessMatch.p2.connection
         a = chessMatch.p1.address
         c = chessMatch.p2.connection
         self.players
+        
+        data =  pickle.dumps(["B"] + chessMatch.board)
+        p1c.send(data)
+        p2c.send(data)
+        
         while True:
-            data = c.recv(1024)
-            print("Data received")
-            for connection in self.players:
-                connection.send(bytes(data))
-            if not data:
-                self.players.remove(c)
-                c.close()
-                break
+            #Player 1 makes a move
+            msg = "Y"
+            p1c.send( pickle.dumps(msg) )
+            move = p1c.recv(1024)
+            move = pickle.loads(move)
+            print("Move received from p1")
+            
+            #chessMatch.updateBoard(move)  -- to do
+            
+            #Player 2 makes a move
+            msg = "Y"
+            p2c.send( pickle.dumps(msg) )
+            move = p2c.recv(1024)
+            move = pickle.loads(move)
+            print("Move received from p2")
+            
+            #chessMatch.updateBoard(move) -- to do
+            
+            
+            
 
-    def close(self):
-        # Close the socket
-        self.sock.close()
+
+
+    
